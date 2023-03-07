bddeebe540131daf91f8c11774a838a7e995136a
liyu <liyu@iftech.io>
2023-03-07 21:36:54 +0800

    feat: health middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { NextFunction, Request, Response } from 'express'
 import { AppModule } from './app.module'
+
+function HealthMiddleware(req: Request, res: Response, next: NextFunction) {
+  if (req.path !== '/health') {
+    return next()
+  }
+  res.json({ success: true })
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.use(HealthMiddleware)
+
   await app.listen(3000)
 }
 bootstrap()
